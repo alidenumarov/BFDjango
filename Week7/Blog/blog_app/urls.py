31cd8afb72a6cdee905cdb43b7cadc1f677fd4fa
Aliden Umarov <um_aliden_97@mail.ru>
2018-10-12 16:30:46 +0600

    user(anon too) can add post

--- Week7/Blog/blog_app/urls.py
+++ Week7/Blog/blog_app/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.blog, name="blog")
+    path('', views.blog, name="blog"),
+    path('add_blog', views.add_block, name="add_blog"),
 ]
--- Week7/Blog/blog_app/views.py
+++ Week7/Blog/blog_app/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect
-
+from . forms import PostForm, CommentForm
 from . models import Post, Comment
 
 # Create your views here.
@@ -7,4 +7,17 @@ from . models import Post, Comment
 def blog(request):
     if(request.method == "GET"):
         posts = Post.objects.all()
-    return render(request, 'blog.html', {"posts": posts})    
+    return render(request, 'blog.html', {"posts": posts})
+
+def add_block(request):
+    if (request.method == 'POST'):
+        if request.user.is_authenticated:
+            author = request.user.username
+        else:
+            author = "anonymous author"
+        title = request.POST['title']
+        text =  request.POST['text']
+        post = Post(author = author, title = title, text = text)
+        post.save()
+        return redirect('/blog')
+    return render(request, 'add_blog.html')
